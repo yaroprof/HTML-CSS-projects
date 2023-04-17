6b7c4f073ff02e3c235e39eb7751b8a2e68ae811
yaroprof <crosskyiv@gmail.com>
2023-04-17 18:17:49 +0300

    dark-mode commit

--- HTML-Features/Headers/e-comerce01/script.js
+++ HTML-Features/Headers/e-comerce01/script.js
@@ -1,7 +1,15 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const navList = document.querySelector('nav ul');
 
+// burger-button menu
 burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('active');
   navList.classList.toggle('active');
+});
+
+// togle change background color
+const body = document.querySelector('body');
+const toggleColor = document.querySelector('.btn-dark');
+toggleColor.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
 });
